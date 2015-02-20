ed475f7db9d66c41e59ef53c4bd0822deb1b0988
fernandofmendes <fernando.mendes@webca.com.br>
2015-02-20 17:54:42 -0200

    up files

---
+++ public/admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://babioli.com.br/admin/');
+define('HTTP_CATALOG', 'http://babioli.com.br/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://babioli.com.br/admin/');
+define('HTTPS_CATALOG', 'http://babioli.com.br/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/fernandomendes/github/babioli/public/admin/');
+define('DIR_SYSTEM', '/Users/fernandomendes/github/babioli/public/system/');
+define('DIR_DATABASE', '/Users/fernandomendes/github/babioli/public/system/database/');
+define('DIR_LANGUAGE', '/Users/fernandomendes/github/babioli/public/admin/language/');
+define('DIR_TEMPLATE', '/Users/fernandomendes/github/babioli/public/admin/view/template/');
+define('DIR_CONFIG', '/Users/fernandomendes/github/babioli/public/system/config/');
+define('DIR_IMAGE', '/Users/fernandomendes/github/babioli/public/image/');
+define('DIR_CACHE', '/Users/fernandomendes/github/babioli/public/system/cache/');
+define('DIR_DOWNLOAD', '/Users/fernandomendes/github/babioli/public/download/');
+define('DIR_LOGS', '/Users/fernandomendes/github/babioli/public/system/logs/');
+define('DIR_CATALOG', '/Users/fernandomendes/github/babioli/public/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'babioli');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ public/config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://babioli.com.br/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://babioli.com.br/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/fernandomendes/github/babioli/public/catalog/');
+define('DIR_SYSTEM', '/Users/fernandomendes/github/babioli/public/system/');
+define('DIR_DATABASE', '/Users/fernandomendes/github/babioli/public/system/database/');
+define('DIR_LANGUAGE', '/Users/fernandomendes/github/babioli/public/catalog/language/');
+define('DIR_TEMPLATE', '/Users/fernandomendes/github/babioli/public/catalog/view/theme/');
+define('DIR_CONFIG', '/Users/fernandomendes/github/babioli/public/system/config/');
+define('DIR_IMAGE', '/Users/fernandomendes/github/babioli/public/image/');
+define('DIR_CACHE', '/Users/fernandomendes/github/babioli/public/system/cache/');
+define('DIR_DOWNLOAD', '/Users/fernandomendes/github/babioli/public/download/');
+define('DIR_LOGS', '/Users/fernandomendes/github/babioli/public/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'babioli');
+define('DB_PREFIX', 'oc_');
+?>
